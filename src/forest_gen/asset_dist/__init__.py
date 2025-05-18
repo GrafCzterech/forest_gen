5cc552a33315e2651ca76391d3ec12aa92c6721f
gogosnoby <oskarwiniarski09@gmail.com>
2025-05-18 16:53:48 +0200

    Basic grass distribution prototype

--- src/forest_gen/asset_dist/__init__.py
+++ src/forest_gen/asset_dist/__init__.py
@@ -1,5 +1,6 @@
 from .sim import Simulation
 from .state import SimulationState
 from .definitions import Species, Plant
+from .grass import grass_points
 
-__all__ = ["Simulation", "Species", "Plant", "SimulationState"]
+__all__ = ["Simulation", "Species", "Plant", "SimulationState", "grass_points"]
