7a030789dfe1f3b3713b8a1f36a8f53cbe54d562
Jager72 <{ID}+{username}@users.noreply.github.com>
2025-06-20 23:05:36 +0200

    temp: small hack for temp work

--- src/forest_gen/__init__.py
+++ src/forest_gen/__init__.py
@@ -1,3 +1,32 @@
-from .scene import ForestGenSpec
+"""
+forest_gen – procedural forest-generation toolkit
+"""
 
-__all__ = ["ForestGenSpec"]
+from importlib import metadata as _metadata
+
+try:
+    __version__ = _metadata.version(__name__)
+except _metadata.PackageNotFoundError:         
+    __version__ = "0.0.0.dev0"
+
+from .temp import TerrainGenerator, TerrainConfig, TerrainBuilder
+# from .asset_dist import AssetDistributor          # <- adjust name if needed
+# from .heightmap import HeightMap
+# from .travelvisibilitymap import TravelVisibilityMap  # if the module exists
+
+__all__ = [
+    "TerrainGenerator",
+    "TerrainConfig",
+    "TerrainBuilder",
+    "__version__",
+]
+
+    # "AssetDistributor",
+    # "HeightMap",
+    # "TravelVisibilityMap",
+
+
+
+# from .scene import ForestGenSpec
+
+# __all__ = ["ForestGenSpec"]
