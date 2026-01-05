8a99b95e89393306341344c796b1cb890c8363e4
Neerka <kuba.markil0220@gmail.com>
2026-01-05 23:04:06 +0100

    example simple training task to run

--- task/task.py
+++ task/task.py
@@ -7,20 +7,35 @@ from isaaclab_tasks.manager_based.locomotion.velocity.config.spot.flat_env_cfg i
     SpotEventCfg,
     SpotObservationsCfg,
     SpotRewardsCfg,
-    SpotTerminationsCfg,
 )
+import isaaclab_tasks.manager_based.locomotion.velocity.mdp as mdp
+from isaaclab.managers import TerminationTermCfg as DoneTerm
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.utils import configclass
+
 from stripe_kit import TrainingSpec
 
 from forest_gen import ForestGenSpec
 
+@configclass
+class TerminationsCfg:
+    """Termination terms for the MDP."""
+
+    time_out = DoneTerm(func=mdp.time_out, time_out=True)
+    body_contact = DoneTerm(
+        func=mdp.illegal_contact,
+        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names=["body", ".*leg"]), "threshold": 1.0},
+    )
+
+
 task_spec = TrainingSpec(
-    scene=ForestGenSpec(asset_path="./models", size=50),
+    scene=ForestGenSpec(asset_path="../models", size=50),
     robot=SPOT_CFG,
     actions=SpotActionsCfg(),
     observations=SpotObservationsCfg(),
     events=SpotEventCfg(),
     rewards=SpotRewardsCfg(),
-    terminations=SpotTerminationsCfg(),
+    terminations=TerminationsCfg(),
     commands=SpotCommandsCfg(),
     sensors={
         "contact_forces": ContactSensorCfg(
